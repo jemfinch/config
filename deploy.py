a5a1ab20e7054eb560511abdc46c7bfaa5597793
Jeremy Fincher <jfincher@jfincher-glaptop.(none)>
2010-10-12 11:08:00 -0700

    Fixed deploy.py to actually work.

--- deploy.py
+++ deploy.py
@@ -1,17 +1,27 @@
 #!/usr/bin/env python
 
-import os
+import os, sys
 
 def homify(*args):
-  return os.path.join(os.environ('HOME'), *args)
+  return os.path.normpath(os.path.join(os.environ['HOME'], *args))
 
 for (dirpath, dirnames, filenames) in os.walk(os.curdir):
+  try:
+    dirnames.remove('.git')
+  except ValueError:
+    pass
   for dirname in dirnames:
     hdirname = homify(dirpath, dirname)
     if not os.path.exists(hdirname):
       os.makedirs(hdirname)
   for filename in filenames:
     filename = os.path.join(dirpath, filename)
+    rfilename = os.path.normpath(os.path.join(os.getcwd(), filename))
     hfilename = homify(filename)
-    if not os.path.exists(hfilename):
-      os.symlink(filename, hfilename)
+    if not os.path.exists(hfilename): # True in case of bad symlinks, as well.
+      os.remove(hfilename)
+      os.symlink(rfilename, hfilename)
+    elif os.path.islink(hfilename) and os.readlink(hfilename) != rfilename:
+      print >>sys.stderr, \
+            'Wanted to create symlink %s => %s but file existed.' % \
+            (rfilename, hfilename)
