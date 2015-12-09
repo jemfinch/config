dbb4f97bd5c8e0b8d2eca6f149475d16a6e95c4d
Jeremy Fincher <jeremy@finchers.us>
2015-12-09 08:31:08 -0800

    Ensure emacs keybindings work with Cinnamon (Ubuntu 14.04).

--- scripts/gnome-set-emacs-keybindings.sh
+++ scripts/gnome-set-emacs-keybindings.sh
@@ -2,3 +2,4 @@
 
 gconftool-2 --set /desktop/gnome/interface/gtk_key_theme Emacs --type string
 gsettings set org.gnome.desktop.interface gtk-key-theme "Emacs"
+gsettings set org.cinnamon.desktop.interface gtk-key-theme "Emacs"
